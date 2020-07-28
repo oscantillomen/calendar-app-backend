03f38a0535d7840156d3e172c9fe88d0e484e4e2
oscar <oscantillomen@gmail.com>
2020-07-28 09:10:44 -0500

    calendar app v1

---
+++ public/precache-manifest.4931a2e99715abcf13035ddebd972d21.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da7c81a8e167b34a08bbb86965e49f6a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a62ad79075ae30ffa5d2",
+    "url": "/static/css/2.dbd11c43.chunk.css"
+  },
+  {
+    "revision": "a601f1b65b9935c2ca45",
+    "url": "/static/css/main.976afbbf.chunk.css"
+  },
+  {
+    "revision": "a62ad79075ae30ffa5d2",
+    "url": "/static/js/2.ebb759eb.chunk.js"
+  },
+  {
+    "revision": "fa4b4056bba234820139274405351863",
+    "url": "/static/js/2.ebb759eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a601f1b65b9935c2ca45",
+    "url": "/static/js/main.3e17d8c9.chunk.js"
+  },
+  {
+    "revision": "7e829d7f4f2411c25d99",
+    "url": "/static/js/runtime-main.8c2230a5.js"
+  }
+]);
